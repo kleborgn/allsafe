78acefa4069663bb9ebad58e583b0814d025f492
Killian Monnier <paraceltus@gmail.com>
2020-05-02 01:16:26 +0200

    à bat bootstrap

---
+++ js/animation.js
@@ -0,0 +1,16 @@
+/**
+ * @author Kilian Le Borgne & Killian Monnier
+ * @see https://github.com/kleborgn/allsafe
+ * @copyright MIT Licence © 2020 Kilian LE BORGNE & Killian MONNIER
+ * @description Script d'animation pour le website AllSafe
+ */
+
+$(document).ready(function(){
+    $('a').on('click', function(evt){ // au clic sur un lien
+        evt.preventDefault(); // bloquer le comportement par défaut: on ne rechargera pas la page
+        var target = $(this).attr('href'); // enregistre la valeur de l'attribut  href dans la variable target
+        $('html, body') // le sélecteur $(html, body) permet de corriger un bug sur chrome et safari (webkit)
+        .stop() // on arrête toutes les animations en cours 
+        .animate({scrollTop: $(target).offset().top}, 1000 ); // on fait maintenant l'animation vers le haut (scrollTop) vers notre ancre target
+    });
+});
