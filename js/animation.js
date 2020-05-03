53c9047e9475e8475eada7898e3f6bf765862ae9
Killian Monnier <paraceltus@gmail.com>
2020-05-03 19:36:26 +0200

    add clients and aside / fix responsive in services

--- js/animation.js
+++ js/animation.js
@@ -6,11 +6,20 @@
  */
 
 $(document).ready(function(){
+    /**
+     * Go to anchor smoothly
+     */
     $('a').on('click', function(evt){ // au clic sur un lien
         evt.preventDefault(); // bloquer le comportement par défaut: on ne rechargera pas la page
         var target = $(this).attr('href'); // enregistre la valeur de l'attribut  href dans la variable target
+
         $('html, body') // le sélecteur $(html, body) permet de corriger un bug sur chrome et safari (webkit)
-        .stop() // on arrête toutes les animations en cours 
-        .animate({scrollTop: $(target).offset().top}, 1000 ); // on fait maintenant l'animation vers le haut (scrollTop) vers notre ancre target
+            .stop() // on arrête toutes les animations en cours 
+            .animate({scrollTop: $(target).offset().top}, 1000); // on fait maintenant l'animation vers le haut (scrollTop) vers notre ancre target
     });
+
+    /**
+     * Parallax
+     */
+    $('#code-parallax').parallax("center", 0, 0.1, true);
 });
